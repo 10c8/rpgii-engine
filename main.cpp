779a94404e1c8b3a6a2a63effa1c38caec19b356
VoxelSquare <williampigmeu@gmail.com>
2012-10-27 12:49:51 -0200

    A lot of additions in the code!
    Added test GUI, a simple function to parse the keys (not useful yet),
    and some minor things.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
  * RPGii Engine
  * ASCII-based open-source RPG game engine
  *
- * @copyright GPL © 2007
+ * @copyright GPL (c) 2007
 **/
 
 #include <iostream>
@@ -16,6 +16,13 @@ int Key;
 
 // "Game" config
 const char* GameTitle = "RPGii Engine - Tests!";
+
+/* Game Tiles */
+char tGUI[6][60] = {"+---------------------------------------------------------+",
+				    "| RPGii Engine | [E]quipment [I]nventory [M]inimap [Q]uit |",
+				    "+---------------------------------------------------------+",
+                    "| HP: 100 | MP: 100 | Atk/Def: 01/01 | Exp: 000 | Lvl: 01 |",
+                    "+---------------------------------------------------------+"};
 
 /* Main engine code */
 void MoveCur(int cX, int cY)
@@ -32,26 +39,71 @@ void Colous(int cO)
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), cO);
 }
 
-void ParseKeys(){
-	// Nothing yet
+void DrawGUI()
+{
+	MoveCur(0,20);
+	
+	for(int i = 0; i < 6; i++)
+	{
+		for(int j = 0; j < 60; j++)
+		{
+			cout << tGUI[i][j];
+		}
+		cout << endl;
+	}
+}
+
+void ParseKeys()
+{
+	switch(Key)
+	{
+		/* Commands */
+		case 101: // E
+			// Nothing yet
+		break;
+		
+		case 105: // I
+			// Nothing yet
+		break;
+		
+		case 109: // M
+			// Nothing yet
+		break;
+		
+		/* Movement and actions */
+		case 119: // W
+			// Nothing yet
+		break;
+		
+		case 115: // S
+			// Nothing yet
+		break;
+		
+		case 97: // A
+			// Nothing yet
+		break;
+		
+		case 100: // D
+			// Nothing yet
+		break;
+	}
 }
 
 void DoTest();
-
 int main()
 {
 	SetConsoleTitle(GameTitle);
 	
-	DoTest();
+	DrawGUI();
 	
-    while(Key != 27)
-    {
+	while(Key != 113)
+	{
 		if(kbhit()){Key = getch(); ParseKeys();}
 		
 		MoveCur(0,0);
 		Sleep(100);
-    }
-    return 0;
+	}
+	return 0;
 }
 
 /* The testing area! :D */
